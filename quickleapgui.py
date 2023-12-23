4fd752b658c3412c6be10d790ad5c380800a1675
Abhinav <abhi74k@stanford.edu>
2023-12-23 15:31:42 -0500

    New widget based on textbox which supports text highlighting

--- quickleapgui.py
+++ quickleapgui.py
@@ -1,30 +1,28 @@
-"""
-"""
-
-import sys
 from tkinter import *
-from tkinter.ttk import *
+
 import time
+import sys
 
 from fuzzystringmatch import fuzzy_regex_match_window_list
 from windows_utils import get_all_windows
 
 
-class EntryWithListBox(Entry):
+class TextAsListBox(Entry):
+
     def __init__(self, *args, **kwargs):
+        self.default_text = 'Enter window title...'
+        self.var = kwargs['textvariable'] = StringVar(value=self.default_text)
+
         Entry.__init__(self, *args, **kwargs)
 
         self.default_window_list = None
-        self.default_text = 'Enter window title...'
-
-        self.var = self["textvariable"]
-        if self.var == '':
-            self.var = self["textvariable"] = StringVar(value=self.default_text)
 
         self.item_height = 18  # Approximate pixel height of one item in the listbox
         self.extra_height = 5
-        self.window_width = 500
+        self.window_width = 700
         self.max_height = 16
+
+        self.current_row_index = None
 
         self.listboxUp = False
         self.root = args[0]
@@ -33,20 +31,20 @@ class EntryWithListBox(Entry):
         self.selection_range(0, END)
 
         self.var.trace('w', self.on_entry_change)
-        self.bind("<Right>", self.selection)
         self.bind("<Up>", self.moveUp)
         self.bind("<Down>", self.moveDown)
-        self.bind("<Return>", self.selection)
+        self.bind("<Return>", self.switch_to_selected_window)
 
     def on_entry_change(self, name, index, mode):
+        print(f"on_entry_change")
 
-        # We should not react to default text
+        matches = []
+
         if self.var.get() == self.default_text:
             if self.listboxUp:
-                self.listbox.destroy()
+                self.textbox.destroy()
                 self.listboxUp = False
         else:
-
             # Display all open windows when no text is entered
             if self.var.get().strip() == '':
                 if self.default_window_list is None:
@@ -58,40 +56,73 @@ class EntryWithListBox(Entry):
                 self.words = self.default_window_list
             else:
                 print(f"Filtered window list")
-                self.words = self.comparison()
+                matches, self.words = self.comparison()
 
-            if self.words:
-                if not self.listboxUp:
-                    listbox_height = min(len(self.words), self.max_height)
+        print(f"# selected links: {len(self.words)}")
 
-                    self.scrollbar_x = Scrollbar(self.root)
+        if self.words:
 
-                    self.listbox = Listbox(width=self["width"], height=listbox_height,
-                                           xscrollcommand=self.scrollbar_x.set)
-                    self.listbox.bind("<Button-1>", self.selection)
-                    self.listbox.bind("<Right>", self.selection)
-                    self.listbox.place(x=self.winfo_x(), y=self.winfo_y() + self.winfo_height())
-                    self.listboxUp = True
+            if not self.listboxUp:
+                self.create_textbox()
 
-                    self.scrollbar_x.config(command=self.listbox.xview)
-                    self.scrollbar_x.grid(row=1, column=0)
+        self.clear_textbox()
 
-                    root.geometry(f"{self.window_width}x{listbox_height * self.item_height + self.extra_height}")
+        for i, w in enumerate(self.words):
+            self.textbox.insert(END, "*) " + w.title + "\n")
 
-                self.listbox.delete(0, END)
-                for i, w in enumerate(self.words):
-                    self.listbox.insert(END, w.title)
-            else:
-                if self.listboxUp:
-                    self.listbox.destroy()
-                    self.scrollbar_x.destroy()
-                    self.listboxUp = False
+            if len(matches) > 0:
+                for match in matches[i]:
+                    for w in match:
+                        offset = 3
+                        row_start = w.start() + offset
+                        row_end = w.end() + offset
+                        print(f"match: {w}")
+                        print(f"row_start: {row_start}")
+                        print(f"row_end: {row_end}")
 
-    def selection(self, event):
+                        self.textbox.tag_add("start", f"{i + 1}.{row_start}", f"{i + 1}.{row_end}")
+                        self.textbox.tag_config("start", foreground="red")
+
+        self.textbox.config(state=DISABLED)
+
+    def create_textbox(self):
+        textbox_height = min(len(self.words), self.max_height)
+
+        self.scrollbar_x = Scrollbar(self.root)
+
+        self.textbox = Text(width=self["width"], height=textbox_height,
+                            xscrollcommand=self.scrollbar_x.set)
+
+        self.textbox.bind("<Return>", self.switch_to_selected_window)
+        self.textbox.bind("<Up>", self.moveUp)
+        self.textbox.bind("<Down>", self.moveDown)
+        self.textbox.place(x=self.winfo_x(), y=self.winfo_y() + self.winfo_height())
+        self.textbox.tag_configure("highlight", background="LightSkyBlue1")
+        self.listboxUp = True
+
+        self.scrollbar_x.config(command=self.textbox.xview)
+        self.scrollbar_x.grid(row=1, column=0)
+
+        root.geometry(f"{self.window_width}x{textbox_height * self.item_height + self.extra_height}")
+
+    def clear_textbox(self):
+
+        if self.current_row_index is not None:
+            self.remove_highlight(self.current_row_index)
+
+        self.current_row_index = None
+
+        self.textbox.config(state=NORMAL)
+
+        self.textbox.delete("1.0", END)
+
+    def switch_to_selected_window(self, event):
+        print(f"switch to selected window")
+
         if self.listboxUp:
-            selected_index = self.listbox.curselection()[0]
+            selected_index = self.current_row_index - 1
 
-            self.listbox.destroy()
+            self.textbox.destroy()
             self.listboxUp = False
 
             root.geometry(f"{self.window_width}x{self.winfo_height()}")
@@ -107,47 +138,67 @@ class EntryWithListBox(Entry):
             time.sleep(2)
             sys.exit(0)
 
+        print(f"index: {self.textbox.index(INSERT)}")
+        row_index = int(self.textbox.index(INSERT).split('.')[0])
+        print(f"row_index: {row_index}")
+        self.highlight(row_index)
+
+    def selection(self, event):
+        print(f"selection")
+
+        print(f"index: {self.textbox.index(INSERT)}")
+        row_index = int(self.textbox.index(INSERT).split('.')[0])
+        print(f"row_index: {row_index}")
+        self.highlight(row_index)
+
+    def highlight(self, n):
+        if self.current_row_index is not None:
+            self.remove_highlight(self.current_row_index)
+
+        self.textbox.tag_add("highlight", "{}.0".format(n), "{}.0+1lines".format(n))
+        self.current_row_index = n
+        self.textbox.mark_set("current", "{}.0".format(n))
+        self.textbox.see("current")
+
+    def remove_highlight(self, n):
+        self.textbox.tag_remove("highlight", "{}.0".format(n), END)
+
     def moveUp(self, event):
-        if self.listboxUp:
-            if self.listbox.curselection() == ():
-                index = '0'
-            else:
-                index = self.listbox.curselection()[0]
+        if self.current_row_index is None or self.current_row_index == 1:
+            next_index = len(self.words)
+        else:
+            next_index = self.current_row_index - 1
 
-            if index != '0':
-                self.listbox.selection_clear(first=index)
-                index = str(int(index) - 1)
-
-                self.listbox.see(index)  # Scroll!
-                self.listbox.selection_set(first=index)
-                self.listbox.activate(index)
+        self.highlight(next_index)
 
     def moveDown(self, event):
-        if self.listboxUp:
-            if self.listbox.curselection() == ():
-                index = '-1'
-            else:
-                index = self.listbox.curselection()[0]
+        print(f"moveDown")
 
-            if index != END:
-                self.listbox.selection_clear(first=index)
-                index = str((int(index) + 1) % self.listbox.size())
-                self.listbox.see(index)  # Scroll!
-                self.listbox.selection_set(first=index)
-                self.listbox.activate(index)
+        if self.current_row_index is None or self.current_row_index == len(self.words):
+            next_index = 1
+        else:
+            next_index = self.current_row_index + 1
+
+        print(f"next_index: {next_index}")
+        self.highlight(next_index)
 
     def comparison(self):
         query_string = self.var.get()
+        print("Query string: " + query_string)
+
         matched_list = fuzzy_regex_match_window_list(query_string, self.default_window_list)
-        ret = [match[1] for match in matched_list]
-        return ret
+        matches = [[match[0]] for match in matched_list]
+        windows = [match[1] for match in matched_list]
+        return matches, windows
 
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Quickleap')
 
-    entry = EntryWithListBox(root, width=100)
+    root.geometry(f"700x200")
+
+    entry = TextAsListBox(root, width=700)
     entry.grid(row=0, column=0)
 
     root.mainloop()
